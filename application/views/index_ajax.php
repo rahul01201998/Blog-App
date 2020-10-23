778d94c0a74efd060a044dc4297bcc67e4bbc908
rahul01201998 <rahul.nachare@ethinos.com>
2020-10-23 13:54:59 +0530

    add final changes

--- application/views/index_ajax.php
+++ application/views/index_ajax.php
@@ -1,7 +1,7 @@
 
 <!-- xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx -->
 <?php if( ! empty($products)) { ?>
-			<?php foreach($products as $product){ ?>
+      <?php foreach($products as $product){ ?>
 <div class="container bg-light mt-4" style="border: 1px solid cyan; border-radius: 10px;box-shadow: 5px 2px 7px 4px #888888;
 ">
         <div class="row">
@@ -9,9 +9,14 @@
             <label><b>Author :-</b></label> <span><?php echo $product->author ?></span>
           </div>
           <div class="col-md-6 col-sm-6 col-6 text-right">
-            <label><b>Action :-</b></label>
+            
+            <?php if($this->session->userdata('username') == 'admin')  {?>
+              <label><b>Action :-</b></label>
             <a href="javascript:;" class="item-edit" data="<?php echo $product->id ?>">Edit</a>
             <a href="javascript:;" class="item-delete" data="<?php echo $product->id ?>">Delete</a>
+          <?php }else{ ?>
+
+          <?php } ?>
           </div>
           <div class="col-md-12 text-center">
             <label><b>Blog Description</b></label>
@@ -26,14 +31,14 @@
         </div>
       </div>
       <?php } ?>
-			<?php } else { ?>
-				<center>No records</center>
-			<?php } ?>
+      <?php } else { ?>
+        <center>No records</center>
+      <?php } ?>
 <!-- xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx -->
 <div class="container mt-3">
-	<div class="box-footer">
-	<ul class="pagination">
-		<?php echo $page ?>
-	</ul>
+  <div class="box-footer">
+  <ul class="pagination">
+    <?php echo $page ?>
+  </ul>
 </div>
 </div>
